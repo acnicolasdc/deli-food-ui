b9497839632cef2936f67414059fd89238abd4bc
NICOLAS REYES <nicolasreyesbejarano@gmail.com>
2024-09-10 12:57:54 -0500

    feat: new distribution

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -7,6 +7,13 @@ import location from '../../../public/your-location.png';
 import cappuccino from '../../../public/cappuccino.png';
 import cocktail from '../../../public/cocktail.png';
 import cottage from '../../../public/cottage.png';
+
+import collard from '../../../public/collard.png';
+import international from '../../../public/international.png';
+import ramen from '../../../public/ramen.png';
+import sushi from '../../../public/sushi.png';
+
+
 import egg from '../../../public/egg.png';
 import beagle from '../../../public/beagle.png';
 import burger from '../../../public/burger.png';
@@ -71,27 +78,77 @@ export default function Home() {
                   <MixerHorizontalIcon />
                 </Button>
               </DialogTrigger>
-              <DialogContent>
+              <DialogContent className='flex flex-col max-h-[90%] px-0'>
                 <DialogHeader>
-                  <DialogTitle>Filtros</DialogTitle>
+                  <DialogTitle className='text-lg'>Filtros</DialogTitle>
                 </DialogHeader>
-                <div className='flex flex-col w-full px-2 gap-4 pb-4'>
+                <div className='flex flex-col w-full gap-4 pb-4 overflow-hidden overflow-y-auto no-scrollbar  px-6'>
+                  <div className='flex flex-col gap-2'>
+                    <Label className='font-semibold text-md'>
+                      Tipo de producto
+                    </Label>
+                    <p className='text-xs mb-4 text-muted-foreground'>Selecciona que producto buscas</p>
+                    <div className='flex flex-row overflow-hidden overflow-x-auto gap-4 no-scrollbar max-w-full'>
+                      <div className='flex items-center justify-center flex-col space-y-2 h-20 min-w-24 md:h-20 md:min-w-24 border border-slate-100 rounded-lg'>
+                        <Image
+                          src={international}
+                          className="w-6 h-6 md:w-6 md:h-6"
+                          alt='Picture international'
+                        />
+                        <Label className="text-xs">Internacional</Label>
+                      </div>
+                      <div className='flex items-center justify-center flex-col space-y-2 h-20 min-w-24 md:h-20 md:min-w-24 border border-slate-100 rounded-lg'>
+                        <Image
+                          src={burger}
+                          className="w-6 h-6 md:w-6 md:h-6"
+                          alt='Picture burger'
+                        />
+                        <Label className="text-xs">Rapida</Label>
+                      </div>
+                      <div className='flex items-center justify-center flex-col space-y-2 h-20 min-w-24 md:h-20 md:min-w-24 border border-slate-100 rounded-lg'>
+                        <Image
+                          src={sushi}
+                          className="w-6 h-6 md:w-6 md:h-6"
+                          alt='Picture sushi'
+                        />
+                        <Label className="text-xs">Asiatica</Label>
+                      </div>
+                      <div className='flex items-center justify-center flex-col space-y-2 h-20 min-w-24 md:h-20 md:min-w-24 border border-slate-100 rounded-lg'>
+                        <Image
+                          src={ramen}
+                          className="w-6 h-6 md:w-6 md:h-6"
+                          alt='Picture ramen'
+                        />
+                        <Label className="text-xs">Vietnamita</Label>
+                      </div>
+                      <div className='flex items-center justify-center flex-col space-y-2 h-20 min-w-24 md:h-20 md:min-w-24 border border-slate-100 rounded-lg'>
+                        <Image
+                          src={collard}
+                          className="w-6 h-6 md:w-6 md:h-6"
+                          alt='Picture collard'
+                        />
+                        <Label className="text-xs">Vegana</Label>
+                      </div>
+                      
+                    </div>
+                  </div>
+                  <Separator />
                   <div className='space-y-4'>
                     <div>
-                      <Label className='font-semibold'>
-                        Rango de precio
+                      <Label className='font-semibold text-md'>
+                        Presupuesto
                       </Label>
-                      <p className='text-xs mb-4 text-muted-foreground'>Selecciona el valor minimo y maximo a pagar</p>
-                      <div className='flex flex-row items-center gap-4'>
-                        <Input placeholder='Minimo' /><Separator className='w-5' /><Input placeholder='Maximo' />
+                      <p className='text-xs mb-4 text-muted-foreground'>Selecciona el valor maximo a pagar</p>
+                      <div className='w-full'>
+                        <Input placeholder='Ejemplo $150.000' className='w-full' />
                       </div>
                     </div>
-                    <div>
-                      <Label className='font-semibold'>
+                    <div className='pl-2'>
+                      <Label className='font-medium'>
                         Para cuantas personas ?
                       </Label>
-                      <p className='text-xs mb-4 text-muted-foreground'>Buscaremos un restaurante que se ajuste a tu rango de presupuesto por la cantidad de personas</p>
-                      <div className='flex flex-row gap-4 items-center'>
+                      <p className='text-xs mb-4 text-muted-foreground'>Buscaremos un restaurante que se ajuste a tu presupuesto por la cantidad de personas</p>
+                      <div className='flex flex-row gap-6 items-center'>
                         <Button variant="outline" >
                           <MinusIcon className='h-2 w-2' />
                         </Button>
@@ -101,11 +158,11 @@ export default function Home() {
                         </Button>
                       </div>
                     </div>
-                    <div>
-                      <Label className='font-semibold'>
+                    <div className='pl-2'>
+                      <Label className='font-medium'>
                         Tipo de plan
                       </Label>
-                      <p className='text-xs mb-4 text-muted-foreground'>Buscaremos un restaurante que se ajuste a tu rango de presupuesto por la cantidad de personas</p>
+                      <p className='text-xs mb-4 text-muted-foreground'>Cuentanos que esperas conseguir con el presupuesto</p>
                       <div >
                         <Select defaultValue='apple'>
                           <SelectTrigger className="w-full">
@@ -124,13 +181,13 @@ export default function Home() {
                   </div>
                   <Separator />
                   <div className='gap-2'>
-                    <Label className='font-semibold'>
+                    <Label className='font-semibold text-md'>
                       Metodos de Pago
                     </Label>
                     <p className='text-xs mb-4 text-muted-foreground'>Selecciona el valor minimo y maximo a pagar</p>
                     <div className='flex flex-col gap-4'>
-                      <div className="items-center flex space-x-2">
-                        <div className="flex flex-1">
+                      <div className="items-center flex space-x-2 justify-center">
+                        <div className="flex flex-1 items-center">
                           <Label
                             htmlFor="terms1"
                             className='font-normal'
@@ -138,10 +195,10 @@ export default function Home() {
                             Efectivo
                           </Label>
                         </div>
-                        <Checkbox id="terms1" />
+                        <Checkbox className='w-5 h-5' id="terms1" />
                       </div>
-                      <div className="items-center flex space-x-2">
-                        <div className="flex flex-1">
+                      <div className="items-center flex space-x-2 justify-center">
+                        <div className="flex flex-1 items-center">
                           <Label
                             htmlFor="terms2"
                             className='font-normal'
@@ -149,10 +206,10 @@ export default function Home() {
                             Transferencia
                           </Label>
                         </div>
-                        <Checkbox id="terms2" />
+                        <Checkbox className='w-5 h-5' id="terms2" />
                       </div>
-                      <div className="items-center flex space-x-2">
-                        <div className="flex flex-1">
+                      <div className="items-center flex space-x-2 justify-center">
+                        <div className="flex flex-1 items-center">
                           <Label
                             htmlFor="terms3"
                             className='font-normal'
@@ -160,13 +217,13 @@ export default function Home() {
                             Tarjetas credito / cebito
                           </Label>
                         </div>
-                        <Checkbox id="terms3" />
+                        <Checkbox className='w-5 h-5' id="terms3" />
                       </div>
                     </div>
                   </div>
 
                 </div>
-                <DialogFooter>
+                <DialogFooter className='px-6'>
                   <Button variant="delifood">Aplicar</Button>
                 </DialogFooter>
               </DialogContent>
